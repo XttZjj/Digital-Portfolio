2a837d83d59fff53b289fdd3cae5cf6e033b3ff5
xttt <georgeluo@sjtu.edu.cn>
2023-12-01 19:08:07 -0500

    change name

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
+// Import necessary components from antd
+import { Layout, Menu, theme, Carousel, Avatar, Card  } from 'antd';
+import React, { useState } from 'react';
+import Ava from './OrbFull.png'
+import cover from './cover.png'
+import website from './website.png'
 
-function App() {
+const { Header, Content, Footer } = Layout;
+
+// Main component
+const App = () => {
+
+  const contentStyle = {
+    margin: 0,
+    color: '#fff',
+    lineHeight: '100px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
+
+  const { Meta } = Card;
+
+  const { Item } = Menu;
+
+  const links = [
+    {
+      label: 'Home',
+      key: 'link1',
+      url: window.location.href,
+    },
+    {
+      label: 'Github',
+      key: 'link2',
+      url: 'https://github.com/XttZjj',
+    },
+    {
+      label: 'Resume',
+      key: 'link3',
+      url: 'https://drive.google.com/file/d/16O18wz_iMqeRSoNZy__hPbLKI90Mxxvv/view?usp=sharing',
+    },
+  ];
+
+  const [current, setCurrent] = useState('link1');
+
+  const onClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  const onChange = (currentSlide) => {
+    console.log(currentSlide);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout className="layout">
+      <Header style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="demo-logo" />
+        <Menu
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          style={{ display: 'flex', justifyContent: 'flex-end' }}
+          theme="dark"
         >
-          Learn React
+          {links.map((link) => (
+            <Item key={link.key}>
+              <a href={link.url} target="_blank" rel="noopener noreferrer">
+                {link.label}
+              </a>
+            </Item>
+          ))}
+        </Menu>
+      </Header>
+      <div style={contentStyle}>
+        <div class="container">
+          <div class="left">
+            <Avatar size={64} src={Ava} />
+            <h1>Xuteng Luo</h1>
+          </div>
+          <div class="right">
+            <p>
+              Hi, I'm Xuteng, a senior Computer Science student at the University of Michigan. I love developing websites and games. Hope you enjoy my work 💕
+            </p>
+          </div>
+        </div>
+      </div>
+      <Carousel afterChange={onChange} autoplay>
+        <div>
+        <a href={'https://xttt.itch.io/chronocaverns'}>
+          <img src={cover} style={{ width: "100vw", height: "61vh" }}/>
         </a>
-      </header>
-    </div>
+        </div>
+        <div>
+        <a href={'https://glkb.dcmb.med.umich.edu/'}>
+          <img src={website} style={{ width: "100vw", height: "61vh" }}/>
+        </a>
+        </div>
+      </Carousel>
+    </Layout>
   );
-}
+};
 
 export default App;
